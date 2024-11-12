7730c26ad1a7eb1162ba8380b394fa492924e19d
rajaram2003 <rajarambiswal123@gmail.com>
2024-11-12 21:05:18 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import { assets } from "../assets/assets"
+
+
+const Footer = () => {
+  return (
+    <div className="pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden" id="Footer">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div className="w-full md:w-1/3 mb-8 md:mb-0">
+          <img src={assets.logo_dark} alt="" />
+          <p className="text-gray-400 mt-4">Estate: Your trusted real estate platform to explore, buy, rent, or sell properties with personalized insights for finding your perfect home.</p>
+        </div>
+        <div className="w-full md:w-1/5 mb-8 md:mb-0">
+          <h3 className="text-white text-lg font-bold mb-4">Company</h3>
+          <ul className="flex flex-col gap-2 text-gray-400">
+            <a href="#Header" className="hover:text-white">Home</a>
+            <a href="#About" className="hover:text-white">About Us</a>
+            <a href="#Contact" className="hover:text-white">Contact Us</a>
+            <a href="#" className="hover:text-white">Privacy Policy</a>
+          </ul>
+        </div>
+        <div className="w-full md:w-1/3">
+          <h3 className="text-white text-lg font-bold mb-4">Stay Updated with Estate</h3>
+          <p className="text-gray-400 mb-4 max-w-80">Get the latest property listings, market insights, and resources delivered to your inbox weekly.</p>
+          <div className="flex gap-2">
+            <input type="email" placeholder="Enter your email" className="p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto"/>
+            <button className="py-2 px-4 rounded bg-blue-500 text-white hover:bg-blue-600">Join Estate</button>
+          </div>
+        </div>
+      </div>
+      <div className="border-t border-gray-700 py-4 mt-10 text-center text-gray-500">
+        Copyright © 2024 Rajaram - All Right Reserved.
+      </div>
+    </div>
+  )
+}
+
+export default Footer
